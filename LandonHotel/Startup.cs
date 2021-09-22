cd2bc674af62cfb2b08f7e9cedb718e81f1da68c
HerRoyalWeirdo <1826083@ucolcloud.ac.nz>
2021-09-22 15:27:15 +1200

    end of chapter 1 vid 3

--- LandonHotel/Startup.cs
+++ LandonHotel/Startup.cs
@@ -24,6 +24,9 @@ namespace LandonHotel
             services.AddMvc();
             services.AddTransient<IRoomService, RoomService>();
             services.AddTransient<IBookingService, BookingService>();
+
+            //register class in transient scope
+            services.AddTransient<IRoomsRepository, RoomsRepository>();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
